ef0cfa30f66f98ed784dca1f3cb4ce67eea106b1
Vihara Senanayake <viharasenanayake402@gmail.com>
2026-04-22 16:18:04 +0530

    Updated GenericDAO

--- src/main/java/com/SmartCampus/dao/GenericDAO.java
+++ src/main/java/com/SmartCampus/dao/GenericDAO.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.GenericDao to edit this template
- */
+
 package com.SmartCampus.dao;
 
-/**
- *
- * @author IIT campus
- */
 
 import com.SmartCampus.model.BaseModel;
 
