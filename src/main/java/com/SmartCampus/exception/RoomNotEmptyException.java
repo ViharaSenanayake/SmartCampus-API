c36a080d6ec8e329dc95e9a934db3ff8695fec68
Vihara Senanayake <viharasenanayake402@gmail.com>
2026-04-22 16:54:10 +0530

    Updated Room Not EMpty Exception

--- src/main/java/com/SmartCampus/exception/RoomNotEmptyException.java
+++ src/main/java/com/SmartCampus/exception/RoomNotEmptyException.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.SmartCampus.exception;
 
-/**
- *
- * @author IIT campus
- */
+
 public class RoomNotEmptyException extends RuntimeException {
     public RoomNotEmptyException(String message) {
         super(message);
