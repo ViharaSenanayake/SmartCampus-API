9f47e578cb8549e7d51f66ab9d40da78c5fba68b
Vihara Senanayake <viharasenanayake402@gmail.com>
2026-04-23 08:29:59 +0530

    add RoomNotEmptyExceptionMapper for 409 conflict responses

---
+++ src/main/java/com/SmartCampus/exception/RoomNotEmptyExceptionMapper.java
@@ -0,0 +1,25 @@
+
+package com.SmartCampus.exception;
+
+
+import com.SmartCampus.model.ErrorMessage;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+    @Override
+    public Response toResponse(RoomNotEmptyException exception) {
+        ErrorMessage error = new ErrorMessage(
+            409,
+            "ROOM_NOT_EMPTY",
+            exception.getMessage()
+        );
+        return Response.status(Response.Status.CONFLICT)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
